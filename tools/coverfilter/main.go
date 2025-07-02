ced9b1b6b25cdb6a0158c80e28219691573d71c3
sordis <sordis61@gmail.com>
2025-07-02 18:24:42 +0300

    feat: add coverage tool

---
+++ tools/coverfilter/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !shouldIgnore(line) {
+			fmt.Println(line)
+		}
+	}
+}
+
+func shouldIgnore(line string) bool {
+	// Игнорируем stdlib
+	if strings.Contains(line, "/go/src/") {
+		return true
+	}
+
+
+	ignorePhrases := []string{
+		"log.Fatal",
+		"os.Exit",
+		"panic(",
+	}
+
+	for _, phrase := range ignorePhrases {
+		if strings.Contains(line, phrase) {
+			return true
+		}
+	}
+
+	return false
+}
